97a8275a00aec51746044e6f8200ff2bd0f9b82e
Hajar Lkhaili <lkhailihajar@gmail.com>
2023-03-16 17:46:46 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
  *
  * Description: A C program that prints with printf function.
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-Return (0);
+return (0);
 }
